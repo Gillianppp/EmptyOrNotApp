85ac5527e6d1700c03bda569c927c4db4830ef29
Ran Gong <ran.gong@slalom.com>
2019-08-14 09:47:51 -0400

    id feed in now

--- containers/Home.js
+++ containers/Home.js
@@ -32,25 +32,34 @@ export default class Home extends React.Component{
   }
 
   render(){
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+					<Text>Loading Here LOADING LOADING</Text>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
     return (
     <ImageBackground source={require("../assets/images/bg.png")} style={styles.bg}>
       <View style={styles.containerHome}>
-      <FlatList data={this.state.dataSource}
+      {/* <FlatList data={this.state.dataSource}
            renderItem={({item}) => <Text>{item.url}</Text>}
-        />
+        /> */}
         <CardStack
 					loop={true}
 					verticalSwipe={false}
 					renderNoMoreCards={() => null}
 					ref={swiper => (this.swiper = swiper)}
 				>
-					{Demo.map((item, index) => (
+					{this.state.dataSource.map((item, index) => (
 						<Card key={index}>
 							<CardItem
-								image={item.image}
-								name={item.name}
-								description={item.description}
-								matches={item.match}
+								image={item.url}
+								name={item.id}
+								description={item.id}
+								matches={item.id}
 								actions
 								onPressLeft={() => this.swiper.swipeLeft()}
 								onPressRight={() => this.swiper.swipeRight()}
